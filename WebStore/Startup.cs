b7dd39210f757218bd98bd42c363ce302181cd0e
TouchPad <olsher258@mail.ru>
2021-09-14 11:13:50 +0300

    Удаления хлама

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -12,20 +12,14 @@ namespace WebStore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services) => services.AddRazorPages();
+
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
