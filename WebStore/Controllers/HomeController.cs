b9ca035158cf1a503ea962e5631097a39c776009
TouchPad <olsher258@mail.ru>
2021-09-14 12:13:15 +0300

    Первый Контролер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello †¤││¤†");
+        }
+
+        public IActionResult SeconAction(string id)
+        {
+            return Content($"Second action with parameter {id}");
+        }
+    }
+}
