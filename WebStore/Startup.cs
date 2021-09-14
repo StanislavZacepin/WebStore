7903a97b821c2e0441746d943d91b9392b244d67
TouchPad <olsher258@mail.ru>
2021-09-14 13:22:55 +0300

    Добавление динамической рекомпиляции представлений в процессе отладки

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -21,7 +21,7 @@ namespace WebStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
