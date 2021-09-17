02c44b85c696f2d3ad181cb39d6b8788a58744fa
TouchPad <olsher258@mail.ru>
2021-09-17 22:29:08 +0300

    Отображение карточки сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -15,10 +15,24 @@ namespace WebStore.Controllers
         public EmployeesController()
         {
             _Employees = TestData.Employees;
+        }       
+        
+        public IActionResult Index() => View(_Employees);
+
+        public IActionResult Details(int id)
+        {
+            //var employee = _Employees.FirstOrDefault(e => e.Id ==id);
+            var employee = _Employees.SingleOrDefault(e => e.Id ==id);
+            
+            if(employee is null)
+                return NotFound();
+
+            return View(employee);                     
         }
 
-       
-
-        public IActionResult Index() => View(_Employees);
+        public IActionResult TestAction(string Parameter1, int Param2)
+        {
+            return Content($"P1:{Parameter1}-P2:{Param2}");
+        }
     }
 }
