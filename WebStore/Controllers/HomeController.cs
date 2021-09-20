b57d4b59a3f34fd68656f7a9ae977a99f0a6ac42
TouchPad <olsher258@mail.ru>
2021-09-20 13:37:14 +0300

    Добавления контролеров и страниц

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
 
-        public IActionResult Index() => View();                       
+        public IActionResult Index() => View();                 
+             
+       
+
     }
 }
