e90d26b132bb6c21c816e5a20df3c5b770be23b0
TouchPad <olsher258@mail.ru>
2021-09-21 14:46:09 +0300

    CRUD-интерфейс в контроллере сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using WebStore.Data;
 using WebStore.Models;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -14,28 +15,100 @@ namespace WebStore.Controllers
     //[Route("Staff/[action]")]
     public class EmployeesController : Controller
     {
-       
+
         private readonly IEmployeesData _EmployeesData;
         private readonly ILogger<EmployeesController> _Logger;
 
         public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
         {
-            
+
             _EmployeesData = EmployeesData;
             _Logger = Logger;
-        }       
+        }
         //[Route("employees/all"]
         public IActionResult Index() => View(_EmployeesData.GetAll());
-      
-       // [Route("employees/info-{id}")]
+
+        // [Route("employees/info-{id}")]
         public IActionResult Details(int id)
-        {           
+        {
             var employee = _EmployeesData.GetById(id);
-            
-            if(employee is null)
+
+            if (employee is null)
                 return NotFound();
 
-            return View(employee);                     
-        }               
-    }
+            return View(employee);
+        }
+
+        #region Create
+        public IActionResult Create() => View("Edit", new EmployeeViewModel());       
+            
+        #endregion
+        #region Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+                return View(new EmployeeViewModel());
+
+            var employee = _EmployeesData.GetById((int)id);
+            if (employee is null) return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.FirstName,
+                LastName = employee.LastName,
+                Age = employee.Age,
+                Patronymic = employee.Patronymic,
+                AboutTheEmployee = employee.AboutTheEmployee,
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            var employee = new Employee
+            {
+                Id = model.Id,
+                LastName = model.LastName,
+                FirstName = model.Name,
+                Patronymic = model.Patronymic,
+                Age = model.Age,
+                AboutTheEmployee = model.AboutTheEmployee,
+            };
+
+            if (employee.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Update(employee);
+
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            if (id < 0) return BadRequest();
+
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null) return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.FirstName,
+                LastName = employee.LastName,
+                Age = employee.Age,
+                Patronymic = employee.Patronymic,
+                AboutTheEmployee = employee.AboutTheEmployee,
+            });
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _EmployeesData.Delete(id);
+            return RedirectToAction(nameof(Index));
+        } 
+        #endregion
+    } 
 }
