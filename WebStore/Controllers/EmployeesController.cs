a49cc11867eb40119b85a8952eff33952ce58c83
TouchPad <olsher258@mail.ru>
2021-09-21 12:54:27 +0300

    Рефакторинг контроллера управления сотрудниками

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
 using WebStore.Models;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Controllers
 {
@@ -12,30 +14,28 @@ namespace WebStore.Controllers
     //[Route("Staff/[action]")]
     public class EmployeesController : Controller
     {
-        private readonly IEnumerable<Employee> _Employees;
+       
+        private readonly IEmployeesData _EmployeesData;
+        private readonly ILogger<EmployeesController> _Logger;
 
-        public EmployeesController()
+        public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
         {
-            _Employees = TestData.Employees;
+            
+            _EmployeesData = EmployeesData;
+            _Logger = Logger;
         }       
         //[Route("employees/all"]
-        public IActionResult Index() => View(_Employees);
+        public IActionResult Index() => View(_EmployeesData.GetAll());
       
        // [Route("employees/info-{id}")]
         public IActionResult Details(int id)
-        {
-            //var employee = _Employees.FirstOrDefault(e => e.Id ==id);
-            var employee = _Employees.SingleOrDefault(e => e.Id ==id);
+        {           
+            var employee = _EmployeesData.GetById(id);
             
             if(employee is null)
                 return NotFound();
 
             return View(employee);                     
-        }
-
-        public IActionResult TestAction(string Parameter1, int Param2)
-        {
-            return Content($"P1:{Parameter1}-P2:{Param2}");
-        }
+        }               
     }
 }
