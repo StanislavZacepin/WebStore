7d9a721e8d91050c934c553ccae97fbae07b538c
TouchPad <olsher258@mail.ru>
2021-09-24 11:23:09 +0300

    Визуальные аннотации данных модели-представления

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +9,17 @@ namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
-
+        [Display(Name = "Имя")]        
         public string Name { get; set; }
-
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
-
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
-
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
-
+        [Display(Name = "О сотруднике")]
         public string AboutTheEmployee { get; set; }
     }
 }
