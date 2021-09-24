4c14a085d5cef0c1250af61d9f67514eba66b38e
TouchPad <olsher258@mail.ru>
2021-09-24 16:47:09 +0300

    Контролер каталогов товара

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -14,7 +15,23 @@ namespace WebStore.Services
 
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
-                  
+            
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IEnumerable<Product> query = TestData.Products;
+
+            //if (Filter?.SectionId != null)
+            //    query = query.Where(p => p.SectionId == Filter.SectionId);
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(p => p.SectionId == section_id);
+
+
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(p => p.BrandId == brand_id);
+
+            return query;
+
+        }
         
     }
 }
