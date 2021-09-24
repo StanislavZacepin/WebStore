6ee00f49bddf9aa118788cc071986e24b08271de
TouchPad <olsher258@mail.ru>
2021-09-24 13:57:38 +0300

    Заготовки визульных компонент

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+   // [ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+       //  public async Task<IViewComponentResult> InvokeAsync() => View();
+        public IViewComponentResult Invoke() => View();
+
+        
+    }
+}
