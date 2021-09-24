87cf1926ddedfda6904a8d65678fc61835aba16b
TouchPad <olsher258@mail.ru>
2021-09-24 11:30:25 +0300

    Настройка связи с браузером

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -38,6 +38,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage(); //Обработка исключений
+                app.UseBrowserLink();
             }
             else
             {
