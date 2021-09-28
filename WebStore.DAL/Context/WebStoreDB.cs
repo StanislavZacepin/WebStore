52e3fdf9a51e7f19d1aef244284b96cd14840651
TouchPad <olsher258@mail.ru>
2021-09-28 13:30:48 +0300

    Определение контекста БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities;
 
 namespace WebStore.DAL.Context
 {
     public class WebStoreDB : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
         public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options) { }
-        //protected override void OnModelCreating(ModelBuilder)
+        //protected override void OnModelCreating(ModelBuilder model)
+        //{
+        //    base.OnModelCreating(model);
+
+        //    //model.Entity<Section>();
+        //}
     }
 }
