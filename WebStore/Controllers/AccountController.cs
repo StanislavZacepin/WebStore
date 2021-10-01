bae78a5af2411a1096635ddee1f73be95f37f480
TouchPad <olsher258@mail.ru>
2021-10-01 11:18:49 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+
+        public IActionResult AccessDenied() => View();
+    }
+}
