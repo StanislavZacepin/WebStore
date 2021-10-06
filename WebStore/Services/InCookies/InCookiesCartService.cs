b99055cb86507d61e60c603b0369bab7a3f52a3a
TouchPad <olsher258@mail.ru>
2021-10-06 19:49:45 +0300

    Основная логика корзины

--- WebStore/Services/InCookies/InCookiesCartService.cs
+++ WebStore/Services/InCookies/InCookiesCartService.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 using WebStore.ViewModels;
 
@@ -15,6 +17,30 @@ namespace WebStore.Services.InCookies
         private readonly IProductData _ProductData;
         private readonly string _CartName;
 
+        private Cart Cart 
+        {
+            get
+            {
+                var context = _HttpContextAccessor.HttpContext;
+                var cookies = context!.Response.Cookies;
+
+                var cart_cookies = context.Request.Cookies[_CartName];
+                if(cart_cookies is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(_CartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+                ReplaceCart(cookies, cart_cookies);
+                return JsonConvert.DeserializeObject<Cart>(cart_cookies);
+            }
+            set => ReplaceCart(_HttpContextAccessor.HttpContext.Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+        private void ReplaceCart(IResponseCookies cookies, string cart)
+        {
+            cookies.Delete(_CartName);
+            cookies.Append(_CartName, cart);
+        }
         public InCookiesCartService(IHttpContextAccessor HttpContextAccessor, IProductData ProductData)
         {
             _HttpContextAccessor = HttpContextAccessor;
@@ -25,14 +51,57 @@ namespace WebStore.Services.InCookies
 
             _CartName = $"WebStore.Cart{user_name}";
         }
-        public void Add(int Id) => throw new NotImplementedException();
+        public void Add(int Id)
+        {
+            var cart = Cart;
 
-        public void Clear() => throw new NotImplementedException();
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null)
+                cart.Items.Add(new CartItem { ProductId = Id, Quantity = 1 });
+            else
+                item.Quantity++;
+            Cart = cart;
+        }
 
-        public void Decrement(int Id) => throw new NotImplementedException();
+        public void Clear()
+        {
+            //  Cart = new();
+            var cart = Cart;
+            cart.Items.Clear();
+            Cart = cart;           
+        }
 
-        public CartViewModel GetViewModel() => throw new NotImplementedException();
+        public void Decrement(int Id)
+        {
+            var cart = Cart;
 
-        public void Remove(int Id) => throw new NotImplementedException();
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null) return;
+
+            if (item.Quantity > 0)
+                item.Quantity--;
+
+            if (item.Quantity <= 0)
+                cart.Items.Remove(item);
+
+            Cart = cart;
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            return null;
+        }
+
+        public void Remove(int Id)
+        {
+            var cart = Cart;
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null) return;
+
+            cart.Items.Remove(item);
+
+            Cart = cart;
+        }
     }
 }
