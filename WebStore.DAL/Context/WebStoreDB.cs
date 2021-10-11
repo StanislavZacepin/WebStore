024449d49cefad96b95f32c84adc9265c8deca34
TouchPad <olsher258@mail.ru>
2021-10-11 13:20:05 +0300

    Добавления заказа в контекст БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Indentity;
+using WebStore.Domain.Entities.Orders;
 
 namespace WebStore.DAL.Context
 {
@@ -17,6 +18,8 @@ namespace WebStore.DAL.Context
         public DbSet<Section> Sections { get; set; }
 
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Order> Orders { get; set; }
+      // public DbSet<OrderItem> OrderItems { get; set; } // Не обязательно
         public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options) { }
         //protected override void OnModelCreating(ModelBuilder model)
         //{
