3ebf8e5d84a83549f608db0742cd93f4006593db
TouchPad <olsher258@mail.ru>
2021-10-11 13:15:02 +0300

    Модели заказа и элемента заказа

---
+++ WebStore/ViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Description { get; set; }
+    }
+}
