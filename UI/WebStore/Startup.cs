0d575db13f6a17f645764ec8e4fc903b2e4d4a0f
TouchPad <olsher258@mail.ru>
2021-10-12 21:11:13 +0300

    Регистрация тестового клиента

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -11,10 +11,12 @@ using WebStore.Domain.Entities.Indentity;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.Middleware;
 using WebStore.Interfaces.Services;
+using WebStore.Interfaces.TestAPI;
 using WebStore.Services.Data;
 using WebStore.Services.Services.InCookies;
 using WebStore.Services.Services.InMemory;
 using WebStore.Services.Services.InSQL;
+using WebStore.WebAPI.Clients.Values;
 
 namespace WebStore
 {
@@ -97,6 +99,8 @@ namespace WebStore
             services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<ICartService,  InCookiesCartService>();
             services.AddScoped<IOrderService,  SqlOrderService>();
+
+            services.AddHttpClient<IValuesService, ValuesClient>(client => client.BaseAddress = new(Configuration["WebAPI"]));
 
             //services.AddSingleton<IProductData, InMemoryProductData>();
             services.AddSingleton<IBlogsData, InMemoryBlogData>();
