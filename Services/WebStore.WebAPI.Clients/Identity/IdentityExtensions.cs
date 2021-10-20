34f9f8a09a6136f55badcdf8f8fad955ea6719e9
TouchPad <olsher258@mail.ru>
2021-10-20 22:54:40 +0300

    Упращения регистрации клиентов

---
+++ Services/WebStore.WebAPI.Clients/Identity/IdentityExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Indentity;
+
+namespace WebStore.WebAPI.Clients.Identity
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection AddIdentityWebStoreWebAPIClients(this IServiceCollection services)
+        {
+            services.AddHttpClient("WebStoreWebAPIIdentity", (s, client) => client.BaseAddress = new(s.GetRequiredService<IConfiguration>()["WebAPI"]))
+               .AddTypedClient<IUserStore<User>, UsersClient>()
+               .AddTypedClient<IUserRoleStore<User>, UsersClient>()
+               .AddTypedClient<IUserPasswordStore<User>, UsersClient>()
+               .AddTypedClient<IUserEmailStore<User>, UsersClient>()
+               .AddTypedClient<IUserPhoneNumberStore<User>, UsersClient>()
+               .AddTypedClient<IUserTwoFactorStore<User>, UsersClient>()
+               .AddTypedClient<IUserClaimStore<User>, UsersClient>()
+               .AddTypedClient<IUserLoginStore<User>, UsersClient>()
+               .AddTypedClient<IRoleStore<Role>, RolesClient>()
+                ;
+            return services;
+        }
+
+        public static IdentityBuilder AddIdentityWebStoreWebAPIClients(this IdentityBuilder builder)
+        {
+            builder.Services.AddIdentityWebStoreWebAPIClients();
+            return builder;
+        }
+    }
+}
