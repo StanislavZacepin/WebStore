47c1cdb92963c1ded85bb9b0e6a950821cef2c44
TouchPad <olsher258@mail.ru>
2021-11-02 15:11:43 +0300

    Пример игнорирования контроллера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -16,6 +16,8 @@ namespace WebStore.TagHelpers
 
         private const string IgnoreAction = "ws-ignore-action";
 
+        private const string IgnoreController = "ws-ignore-controller";
+
         [HtmlAttributeName("ws-is-active-route-active")]
         public string ActiveCssClass { get; set; } = "active";
 
@@ -37,14 +39,15 @@ namespace WebStore.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var is_ignore_action = output.Attributes.RemoveAll(IgnoreAction);
+            var is_ignore_controller = output.Attributes.RemoveAll(IgnoreController);
 
-            if (IsActive(is_ignore_action))
+            if (IsActive(is_ignore_action, is_ignore_controller))
                 MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
         }
 
-        private bool IsActive(bool IgnoreAction)
+        private bool IsActive(bool IgnoreAction, bool IgnoreController)
         {
             var route_values = ViewContext.RouteData.Values;
 
@@ -56,7 +59,7 @@ namespace WebStore.TagHelpers
             if (!IgnoreAction && Action is { Length: > 0 } action && !string.Equals(action, route_action))
                 return false;
 
-            if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+            if (!IgnoreController && Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
                 return false;
 
             foreach (var (key, value) in RouteValues)
