9af1e9cce71422087fb110ae79c7325d3843027d
TouchPad <olsher258@mail.ru>
2021-11-07 19:02:11 +0300

    Консольный клиент для SignalR

--- Tests/WebStore.TestConsole/Program.cs
+++ Tests/WebStore.TestConsole/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace WebStore.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HubConnectionBuilder();
+            var connection = builder
+               .WithUrl("http://localhost:5000/chat")
+               .Build();
+
+            using var registration = connection.On<string>("MessageFromClient", OnMessageFromClient);
+
+            Console.WriteLine("Готов к подключению.");
+            Console.ReadLine();
+
+            await connection.StartAsync();
+
+            Console.WriteLine("Соединение установлено.");
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+                await connection.InvokeAsync("SendMessage", message);
+            }
+        }
+
+        private static void OnMessageFromClient(string Message)
+        {
+            Console.WriteLine("Message from server: {0}", Message);
         }
     }
+    
 }
